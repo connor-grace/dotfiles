1b27a12a6205f7b2b7a19d01c9fae13260d19d34
connor-grace <connor.grace88@gmail.com>
2024-03-30 00:19:08 -0400

    update dotfiles

---
+++ .config/nvim/lua/cgrace/lazy-init.lua
@@ -0,0 +1,45 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  vim.fn.system({
+	"git",
+	"clone",
+	"--filter=blob:none",
+	"--branch=stable",
+	lazyrepo,
+	lazypath
+  })
+end ---@diagnostic disable-next-line: undefined-field
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+    spec = {
+        --"tpope/vim-sleuth", -- Detect tabstop and shiftwidth automatically
+
+        require("cgrace/plugins/cmp"),
+        require("cgrace/plugins/colorscheme"),
+        require("cgrace/plugins/conform"),
+        require("cgrace/plugins/gitsigns"),
+        require("cgrace/plugins/lspconfig"),
+        require("cgrace/plugins/mini"),
+        require("cgrace/plugins/telescope"),
+        require("cgrace/plugins/todo-comments"),
+        require("cgrace/plugins/treesitter"),
+        require("cgrace/plugins/which-key"),
+
+        --  Uncomment the following line and add your plugins to `lua/cgrace/plugins/*.lua` to get going.
+        --    For additional information, see `:help lazy.nvim-lazy.nvim-structuring-your-plugins`
+        -- { import = "custom.plugins" },
+    },
+    defaults = {
+        lazy = false,
+        version = false,
+    },
+    install = {
+        missing = true,
+        colorscheme = { "tokyonight" },
+    },
+    checker = {
+        enabled = true,
+    },
+})
---
+++ .config/nvim/lua/cgrace/options.lua
@@ -0,0 +1,47 @@
+vim.opt.guicursor = ''
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv 'HOME' .. '/.vim/undodir'
+vim.opt.undofile = true
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+vim.opt.termguicolors = true
+
+vim.opt.wrap = false
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = 'yes'
+vim.opt.colorcolumn = '80'
+vim.opt.isfname:append '@-@'
+
+vim.opt.updatetime = 50
+
+-- Don't show the mode, since it's already in the status line
+vim.opt.showmode = false
+
+-- Sync clipboard between OS and Neovim.
+--  Remove this option if you want your OS clipboard to remain independent.
+--  See `:help 'clipboard'`
+--vim.opt.clipboard = 'unnamedplus'
+
+-- Decrease mapped sequence wait time
+-- Displays which-key popup sooner
+vim.opt.timeoutlen = 300
+
+vim.opt.inccommand = 'split'
+
+vim.opt.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
